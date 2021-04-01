c675cef0a83bfdf41089806dbf8fb9899547da66
Alex <sashamur2000@mail.ru>
2021-04-01 17:40:49 +0300

    Websocket init

--- index.js
+++ index.js
@@ -1,55 +1,99 @@
-const server = require("http");
-const client = require("http");
+const server_i = require("ws");
+const client_i = require("ws");
 const fs = require('fs');
 const port_client = 8080;
 const port_server = 8484;
 let html_s = fs.readFileSync('./server_page.html','utf-8')
 let html_c = fs.readFileSync('./client_page.html','utf-8')
 
+const server = new server_i.Server({port:port_server})
+const client = new client_i.Server({port:port_client})
+
 let response;
 let maps=[];
+var clients = {};
 let gt='Bo1';
 
-server.createServer(async function(req, res){
-    if(req.method == "POST"){
-        let body='';
-        req.on('data', data => {
-            body += data;
-        });
-        req.on('end',() => {
-            res.writeHead(200);
-            if(body == 'get_lobby_info'){
-                response = {
-                    game_type: gt,
-                    maps: maps
-                }
-                res.write(JSON.stringify(response));
-            }else{
-                try{
-                    body = JSON.parse(body)
-                }catch(err){
-                    res.end()
-                    return;
-                }
-                if(body.game_type){
-                    gt = body.game_type
-                }
-                if(body.maps){
-                    maps = body.maps
-                }
-            }
-            res.end();
-        });
-    }else if(req.method == "GET"){
-        res.statusCode = 200;
-        res.setHeader('Content-type', 'text/html');
-        res.write(html_s)
-        res.end();
+server.on('connection',(ws)=>{
+    var id = Math.random();
+    clients[id] = ws;
+    console.log("новое соединение " + id);
+    for (var key in clients) {
+        clients[key].send(html_s);
     }
-}).listen(port_server,()=>{
-    console.log('SERVER: http://127.0.0.1'+':'+port_server);
-});
+  
+    ws.on('message', function(message) {
+      console.log('получено сообщение ' + message);
+        let response;
+        if(message == 'get_lobby_info'){
+            response = {
+                game_type: gt,
+                maps: maps
+            }
+        }else{
+            try{
+                body = JSON.parse(body)
+            }catch(err){
+                return;
+            }
+            if(body.game_type){
+                gt = body.game_type
+            }
+            if(body.maps){
+                maps = body.maps
+            }
+        }
+        for (var key in clients) {
+            clients[key].send(message);
+        }
+    });
+  
+    ws.on('close', function() {
+      console.log('соединение закрыто ' + id);
+      delete clients[id];
+    });
+})
 
+client.on('connection',(ws)=>{
+    var id = Math.random();
+    clients[id] = ws;
+    console.log("новое соединение " + id);
+    for (var key in clients) {
+        clients[key].send(html_c);
+    }
+  
+    ws.on('message', function(message) {
+      console.log('получено сообщение ' + message);
+        let response;
+        if(message == 'get_lobby_info'){
+            response = {
+                game_type: gt,
+                maps: maps
+            }
+        }else{
+            try{
+                body = JSON.parse(body)
+            }catch(err){
+                return;
+            }
+            if(body.game_type){
+                gt = body.game_type
+            }
+            if(body.maps){
+                maps = body.maps
+            }
+        }
+        for (var key in clients) {
+            clients[key].send(message);
+        }
+    });
+  
+    ws.on('close', function() {
+      console.log('соединение закрыто ' + id);
+      delete clients[id];
+    });
+})
+/*
 client.createServer(async function(req, res){
     if(req.method == "POST"){
         let body='';
@@ -69,11 +113,8 @@ client.createServer(async function(req, res){
         });
     }
     if(req.method == "GET"){
-        let body='';
-        req.on('data', data => {
-            body += data;
-        });
-        if(!req.url.match(/jpg/g) || !req.url.match(/png/g) | !req.url.match(/gif/g)){
+        console.log()
+        if(!req.url.match(/jpg/g)){
             res.write(html_c)
             res.end();
         }else{
@@ -91,4 +132,4 @@ client.createServer(async function(req, res){
     }
 }).listen(port_client,()=>{
     console.log('CLIENT http://127.0.0.1'+':'+port_client);
-});
+});*/
